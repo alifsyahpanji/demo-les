06e3590467374f9c885f16e8932a7509cd6ae046
alifsyahpanji <alifsyahpanji@gmail.com>
2023-10-11 15:19:51 +0700

    Menambahkan pengecekan telepon pada proses login

--- client/proseslogin.php
+++ client/proseslogin.php
@@ -7,14 +7,27 @@ $randomnumber = rand(100000000, 999999999);
 $stringnumber = (string) $randomnumber;
 $telepon = "081" . $stringnumber;
 
+$sql_cek = "SELECT telepon FROM akun WHERE telepon = '$telepon' ";
+$run_cek = mysqli_query($conn, $sql_cek);
+$count_cek = mysqli_num_rows($run_cek);
 
-$insert_sql = "INSERT INTO akun (telepon) VALUES ('$telepon')";
-$run_insert_sql = mysqli_query($conn, $insert_sql);
-if ($run_insert_sql) {
-    $last_id = mysqli_insert_id($conn);
-    $_SESSION["id"] = $last_id;
-    header("Location: index.php");
+
+
+if ($count_cek > 0) {
+    header("Location: proseslogin.php");
     die();
+} else {
+    $insert_sql = "INSERT INTO akun (telepon) VALUES ('$telepon')";
+    $run_insert_sql = mysqli_query($conn, $insert_sql);
+    if ($run_insert_sql) {
+        $last_id = mysqli_insert_id($conn);
+        $_SESSION["id"] = $last_id;
+        header("Location: index.php");
+        die();
+    }
 }
+
+
+
 
 ?>
